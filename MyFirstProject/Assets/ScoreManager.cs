58c4b8a4156e56a968d22d120e1f86014bdd39e2
arthur1379 <changhsinlin1206@gmail.com>
2017-02-27 22:29:14 +0800

    Week1
    Week1的進度

---
+++ MyFirstProject/Assets/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    private Text _text;
+    private float _CurrentScore = 0;
+    const string ScorePrefix = "Score : ";
+
+	// Use this for initialization
+	void Start () {
+        _text = this.GetComponent<Text>();
+        _text.text = ScorePrefix + _CurrentScore;
+	}
+
+    public void AddScore(int score)
+    {
+        _CurrentScore += score;
+        _text.text = ScorePrefix + _CurrentScore;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
